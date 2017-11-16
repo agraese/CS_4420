b476647f79a591826486bf44effe8bb02e8d6ba8
agraese <agraese2@gmail.com>
2017-11-16 10:58:53 -0700

    manipulating index csv to include indexes

--- navigate_csv.py
+++ navigate_csv.py
@@ -9,9 +9,40 @@ def main():
     csvfile = open(in_file)
     lines = csvfile.readlines()
 
+    clean_rows = []
     for row in lines:
-        fields = row.split(',')
-        print(fields)
+        if row[0] == '"':
+            continue
+        clean_row = row.strip()
+        fields = clean_row.split(',')
+        if len(fields) < 15:
+            continue
+        clean_rows += [fields]
+
+    for row in clean_rows:
+        print(row)
+
+    in_file2 = 'Animal_Breed_Index.csv'
+    breedfile = open(in_file2)
+    breed_file_lines = breedfile.readlines()
+
+    breed_index = 0
+    for i in range(len(clean_rows[0])):
+        if clean_rows[0][i] == "Animal_Breed":
+            breed_index = i
+            print("found breed index")
+    
+    for row in clean_rows:
+        breed = row[breed_index]
+        for breed in breed_file_lines:
+            breed_string = breed.split(',')[1]
+            index = breed.split(',')[0]
+            if breed_string == breed:
+                row[breed_index] = index
+
+    print(clean_rows)
+    
+
 
 if __name__ == "__main__":
     main()
