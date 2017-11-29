d839cbbf4c4f3dd76e45a4295b0736109e14a12f
Luna <ambaye.luna@gmail.com>
2017-11-29 09:12:43 -0700

    config sqlalchemy

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,15 +1,19 @@
 import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = '/Users/lunaambaye/Desktop/SQLAlchemy-1.1.15/lib/sqlalchemy/databases/test.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
 
-def main():
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
     
-    return render_template('index.html') 
-    
-if __name__ == "__main__":
-    #app.run()  # comment out
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))  # use for Cloud9
+    def __repr__(self):
+        return '<User %r>' % self.username     
 
 
